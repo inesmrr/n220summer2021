bd88e50d0d862c90054b72e8ce1581064a09d65d
inesmrr <imrivera@iu.edu>
2021-05-20 10:12:14 -0400

    counter js

---
+++ labs/lab2/ex1/js/app.js
@@ -0,0 +1,8 @@
+// this is so that the first number in the console is 0
+let cereal = -1;
+
+// outputs number once every frame
+function draw(){
+    cereal = cereal + 1;
+    console.log(cereal);
+}
