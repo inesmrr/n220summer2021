59dc7554956e68ae77202d429cb3354f0ac7945d
inesmrr <imrivera@iu.edu>
2021-05-30 13:45:29 -0400

    grow

--- labs/lab5/ex1/js/app.js
+++ labs/lab5/ex1/js/app.js
@@ -1,28 +1,23 @@
+// get div from html
 let dvSquare = document.getElementById("square");
+
 var size = 100;
 
+// html div styling
 dvSquare.innerHTML = "Hola";
 dvSquare.style.backgroundColor="#42f54b";
 dvSquare.style.width = size + "px";
 dvSquare.style.height = size + "px";
 
+// see div info on console
 console.log(dvSquare);
 
-function grow(){
-    // let size = dvSquare;
-    // if(true) {
-    //     size = size * 1.1;
-    // }
-
-    // dvSquare.style.width = w_resized * '1.1px'; 
-
-    
+// onclick function
+function grow(){  
+    // make square grow each time its clicked by 10%
     size = size * 1.1;
     dvSquare.style.width = size + "px";
     dvSquare.style.height = size + "px";
+    // console.log everytime square is clicked
     console.log("it grew");
-
-    // dvSquare.innerHTML = dvSquare.style.width;
-    // dvSquare.innerHTML = size;
-
 }
